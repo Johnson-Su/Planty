9d98e9a5ff775594a51eedfff612935f5253fa59
Keke Zhang <kekezhang@Kekes-MacBook-Pro.local>
2020-05-22 13:39:31 -0600

    can count eggs on a page almost

--- content.js
+++ content.js
@@ -1,7 +1,9 @@
-alert('Would you like to use Planty?')
+//alert('Would you like to use Planty?')
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     //find every instance of egg, case sensitive
-    const re = new RegExp('egg', 'gi')
+    const re = new RegExp('eggs', 'gi')
     const matches = document.documentElement.innerHTML.match(re)
+    sendResponse({count: matches.length})
 
+    
 })
--- popup.js
+++ popup.js
@@ -3,13 +3,21 @@ document.addEventListener('DOMContentLoaded', function () {
     
     function onclick () {
         //content.js searches page for recipe keywords
+        // query the open tabs to find current tabs
         chrome.tabs.query({currentWindow: true, active: true},
         function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, 'hi')
+            chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
         })
 
         //webscrape the recipe
         //calculate carbon emissions for each ingredient
         //when mouse gpoes
+    }
+
+    // count eggs
+    function setCount (res) {
+        const div = document.createElement('div')
+        div.textContent = '${res.count} eggs'
+        document.body.appendChild(div)
     }
 }, false)
