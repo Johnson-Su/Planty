a801682a7f73d2cc6e7c4d0c00a846441fa1cbd6
Amy <amyh102@gmail.com>
2020-05-22 17:32:09 -0600

    python firebase web scraping

--- test.py
+++ test.py
@@ -1,14 +1,23 @@
 from recipe_scrapers import scrape_me
+from firebase import firebase
 
 # give the url as a string, it can be url from any site listed below
-scraper = scrape_me('https://www.allrecipes.com/recipe/158968/spinach-and-feta-turkey-burgers/')
+scraper = scrape_me('https://www.allrecipes.com/recipe/8814/homemade-chicken-soup/')
 
-print(scraper.title())
+title = scraper.title()
+
+ingred = scraper.ingredients()
 
 
-ing = scraper.ingredients()
-for item in ing:
-    print(item.split(",")[0]) 
+firebase = firebase.FirebaseApplication("https://planty-bb6f2.firebaseio.com/", None)
+data = {'0': len(ingred)}
+for i in range(1, len(ingred) + 1):
+    data.update({str(i): scraper.ingredients()[i - 1].split(",")[0]})
+print (data)
 
-scraper.image()
-scraper.links()
+# firebase.post(title, data)
+test = firebase.get(title, None)
+test_values = next(iter(test.values()))
+print(test_values[0])
+
+
