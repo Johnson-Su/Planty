b15266a479d770942871f64d1efe3cade65c7530
Amy <amyh102@gmail.com>
2020-05-22 13:09:35 -0600

    keke's changes

--- popup.js
+++ popup.js
@@ -1,20 +1,15 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button').addEventListener('click', onclick, false)
+    
+    function onclick () {
+        //content.js searches page for recipe keywords
+        chrome.tabs.query({currentWindow: true, active: true},
+        function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, 'hi')
+        })
 
-'use strict';
-
-let changeColor = document.getElementById('changeColor');
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
-};
+        //webscrape the recipe
+        //calculate carbon emissions for each ingredient
+        //when mouse gpoes
+    }
+}, false)
