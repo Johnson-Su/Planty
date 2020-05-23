52b06c58ac79710a44c1faccf0c7b263c4b2a5e2
Johnson Su <johnson_su@Johnsons-MacBook-Pro.local>
2020-05-22 21:08:55 -0600

    large javascript searching implemented

--- firebase.js
+++ firebase.js
@@ -12,8 +12,8 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-  console.log(firebase);
-  console.log("running")
+  console.log("firebase");
+  console.log("running");
 
   chrome.runtime.onMessage.addListener((msg, sender, resp) => {
       if(msg.commang == "fetch"){
@@ -30,6 +30,43 @@
 
   chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
         function(tabs){
-            alert(tabs[0].url);
-        }
-        );
+        //alert(tabs[0].url);
+  });
+
+var high_carbon_array =["beef","butter","canned tuna","cheese","chicken","duck","egg","goat","honey","lamb","mayonnaise","milk","olive oil","pork","salmon","shrimp","turkey","yogurt"];
+
+// function find_string(){
+//   firebase.database().ref('Homemade Chicken Soup/1').on('value', function(snapshot) {
+//       console.log(snapshot.val());
+//       return snapshot.val();
+//     });
+// }
+//
+// find_string();
+var local_high_carbon_array =[];
+function is_it_high_carbon(){ //returns 1 if it is there
+  //finds the info
+  firebase.database().ref('Homemade Chicken Soup/0').on('value', function(snapshot) {
+    var loopnum = snapshot.val();
+    console.log(loopnum);
+    var j;
+    for(j=1;j<=loopnum;j++){;
+      firebase.database().ref('Homemade Chicken Soup/'+j).on('value', function(snapshot) {
+          var hmmm = snapshot.val();
+          console.log(hmmm);
+          var i;
+          for(i=0;i<18;i++){
+            if(hmmm.indexOf(high_carbon_array[i])!=-1){
+              if(local_high_carbon_array.includes(high_carbon_array[i])!=true){
+                local_high_carbon_array.push(high_carbon_array[i]);
+              }
+            }
+          }
+          return 0;
+        });
+    }
+  });
+  console.log(local_high_carbon_array);
+}
+
+is_it_high_carbon();
